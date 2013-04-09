1bb6cd2222e0b1bd38c33aabf436f7dcd14666d9
c9s <cornelius.howl@gmail.com>
2013-04-09 13:35:22 +0800

    test internal field

--- sqlutils/reflect_test.go
+++ sqlutils/reflect_test.go
@@ -1,18 +1,22 @@
 package sqlutils
 import "testing"
+import "fmt"
 
 type FooRecord struct {
 	Id int `json:"id"`
 	Name string `json:"name"`
 	Type string `json:"type"`
+	Internal int `json:-`
 }
 
 func TestParseColumnNames(t * testing.T) {
 	var columns []string
 	columns = ParseColumnNames( FooRecord{Id:3, Name: "Mary"} )
+
 	if len(columns) == 0 {
 		t.Fail()
 	}
+
 
 	columns = ParseColumnNames( FooRecord{Id:4, Name: "John"} )
 	if len(columns) == 0 {
