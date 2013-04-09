5cdee0c21508ea7b52f3d814922ac0da3196ab0b
c9s <cornelius.howl@gmail.com>
2013-04-09 13:03:33 +0800

    Add gatsby/reflect.go

---
+++ reflect.go
@@ -0,0 +1,27 @@
+package gatsby
+import "fmt"
+import "reflect"
+// import "strings"
+
+func GenerateSQLColumns(val interface{}) ([]string) {
+	t := reflect.ValueOf(val)
+	typeOfT := t.Type()
+	columns := []string{}
+	for i := 0; i < t.NumField(); i++ {
+		var columnName string
+		var field reflect.Value = t.Field(i)
+		var tag reflect.StructTag = typeOfT.Field(i).Tag
+		fmt.Printf("%d: %s %s %s = %v\n", i,
+			typeOfT.Field(i).Name,
+			tag.Get("json"),
+			field.Type(),
+			field.Interface())
+
+		columnName = tag.Get("json")
+		if len(columnName) == 0 {
+			columnName = typeOfT.Field(i).Name
+		}
+		columns = append(columns,columnName)
+	}
+	return columns
+}
