dcdd0c3f3e0f38cb9f957859c63d467a0137af74
c9s <cornelius.howl@gmail.com>
2013-04-14 15:43:53 +0800

    refactor insert sql builder

--- sqlutils/insert.go
+++ sqlutils/insert.go
@@ -1,20 +1,37 @@
 package sqlutils
-// import "fmt"
+import "fmt"
 
 import "reflect"
 import "strings"
 import "strconv"
 import "database/sql"
 
-func BuildInsertClause(val interface{}) (string, []interface{}) {
+func BuildInsertColumnsFromMap(cols map[string]interface{}) (string, []interface{}) {
+	var columnNames []string
+	var valueFields []string
+	var values		[]interface{}
+	var i int = 1
+
+	for col, arg := range cols {
+		columnNames = append(columnNames, col)
+		valueFields = append(valueFields, fmt.Sprintf("$%d", i))
+		values    = append(values, arg)
+		i++
+	}
+	sql := "( " + strings.Join(columnNames, ", ") + " )" +
+		" VALUES ( " + strings.Join(valueFields, ", ") + " )"
+	return sql , values
+}
+
+
+// Build Insert Column Clause from a struct type.
+func BuildInsertColumns(val interface{}) (string, []interface{}) {
 	t := reflect.ValueOf(val).Elem()
 	typeOfT := t.Type()
-	tableName := GetTableName(val)
 
 	var columnNames []string
 	var valueFields []string
 	var values      []interface{}
-
 	var fieldId int = 1
 
 	for i := 0; i < t.NumField(); i++ {
@@ -26,7 +43,6 @@ func BuildInsertClause(val interface{}) (string, []interface{}) {
 			continue
 		}
 
-
 		var attributes = GetColumnAttributesFromTag(&tag)
 
 		// if it's a serial column (with auto-increment, we can simply skip)
@@ -34,26 +50,30 @@ func BuildInsertClause(val interface{}) (string, []interface{}) {
 			continue
 		}
 
-		// TODO: see if we can skip null columns, or simply skip Id column
-
 		columnNames = append(columnNames, *columnName)
 		valueFields = append(valueFields, "$" + strconv.Itoa(fieldId) )
 		values      = append(values, field.Interface() )
 		fieldId++
 	}
-	return "INSERT INTO " + tableName + " ( " + strings.Join(columnNames,", ") + " ) " +
-		" VALUES ( " + strings.Join(valueFields,", ") + " )", values
+	return "( " + strings.Join(columnNames,", ") + " ) " +
+	"VALUES ( " + strings.Join(valueFields,", ") + " )", values
+}
+
+
+func BuildInsertClause(val interface{}) (string, []interface{}) {
+	tableName := GetTableName(val)
+	sql, values := BuildInsertColumns(val)
+	return "INSERT INTO " + tableName + sql, values
 }
 
 func GetReturningIdFromRows(rows * sql.Rows) (int64, error) {
-    var id int64
-    var err error
-    rows.Next()
-    err = rows.Scan(&id)
+	var id int64
+	var err error
+	rows.Next()
+	err = rows.Scan(&id)
 	if err != nil {
 		return -1, err
 	}
-    return id, err
+	return id, err
 }
-
 
