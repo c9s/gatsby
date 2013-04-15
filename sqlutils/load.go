a7548744a57613efa108db426827074b70de682a
c9s <cornelius.howl@gmail.com>
2013-04-15 10:29:03 +0800

    Fix gatsby load method, check rows before scan

--- sqlutils/load.go
+++ sqlutils/load.go
@@ -1,21 +1,37 @@
 package sqlutils
 import "database/sql"
 import "fmt"
+import "errors"
 
 // Load record by primary key value.
 func Load(db *sql.DB, val interface{}, pkId int64) (*Result) {
+	defer func() (*Result) {
+        if err := recover(); err != nil {
+			return NewErrorResult(err.(error),"")
+        }
+		return nil
+    }()
+
+
 	pName := GetPrimaryKeyColumnName(val)
 	if pName == nil {
 		panic("primary key is required.")
 	}
 	sql := BuildSelectClause(val) + fmt.Sprintf(" WHERE %s = $1 LIMIT 1", *pName)
 	rows, err := PrepareAndQuery(db, sql, pkId)
-	rows.Next()
-	err = FillFromRow(val,rows)
+
 	if err != nil {
 		return NewErrorResult(err,sql)
 	}
-	return NewResult(sql)
+
+	if rows.Next() {
+		err = FillFromRow(val,rows)
+		if err != nil {
+			return NewErrorResult(err,sql)
+		}
+		return NewResult(sql)
+	}
+	return NewErrorResult(errors.New("No result"),sql)
 }
 
 func LoadByCols(db *sql.DB, val interface{}, cols map[string] interface{}) (*Result) {
