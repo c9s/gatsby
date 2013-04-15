76e5eb68bf43078f4aa54d4ca9346644f65e390f
c9s <cornelius.howl@gmail.com>
2013-04-15 11:04:36 +0800

    check rows

--- sqlutils/returning.go
+++ sqlutils/returning.go
@@ -1,15 +1,18 @@
 package sqlutils
 import "database/sql"
+import "errors"
 
 // This function fetch the returning ID from result rows
 func GetReturningIdFromRows(rows * sql.Rows) (int64, error) {
 	var id int64
 	var err error
-	rows.Next()
-	err = rows.Scan(&id)
-	if err != nil {
-		return -1, err
+	if rows.Next() {
+		err = rows.Scan(&id)
+		if err != nil {
+			return -1, err
+		}
+		return id, err
 	}
-	return id, err
+	return -1, errors.New("No returning ID")
 }
 
