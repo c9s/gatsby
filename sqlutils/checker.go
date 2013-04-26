82bb7711485305cce6f21e69da2fc44e860b93d9
c9s <cornelius.howl@gmail.com>
2013-04-26 17:01:59 +0800

    Fix checker

--- sqlutils/checker.go
+++ sqlutils/checker.go
@@ -9,15 +9,15 @@ func CheckRequired(val interface{}) (error) {
 
 	for i := 0; i < t.NumField(); i++ {
 		var tag        reflect.StructTag = typeOfT.Field(i).Tag
-		var field      reflect.Value = t.Field(i)
-		var fieldType  reflect.Type = field.Type()
+		var fieldValue reflect.Value = t.Field(i)
+		var fieldType  = typeOfT.Field(i)
 		var attributes map[string]bool = GetColumnAttributesFromTag(&tag)
 		if _ , ok := attributes["required"] ; ok {
 			// check the column value
-			if fieldType.String() == "string" && field.Interface().(string) == "" {
-				return errors.New( fieldType.Name() + " field required.")
-			} else if fieldType.String() == "int" && field.Interface().(int) == 0 {
-				return errors.New( fieldType.Name() + " field required.")
+			if fieldValue.Type().Name() == "string" && fieldValue.Interface().(string) == "" {
+				return errors.New( fieldType.Name + " field required.")
+			} else if fieldValue.Type().Name() == "int" && fieldValue.Interface().(int) == 0 {
+				return errors.New( fieldType.Name + " field required.")
 			}
 		}
 	}
