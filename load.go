f402f6a624b28e096df80f2b279e7c18cbd05bff
c9s <cornelius.howl@gmail.com>
2013-05-11 19:36:54 +0800

    refactor load method

--- load.go
+++ load.go
@@ -4,18 +4,11 @@ import "database/sql"
 import "fmt"
 import "gatsby/sqlutils"
 
-// Load record by primary key value.
-func Load(db *sql.DB, val interface{}, pkId int64) *Result {
-	pName := sqlutils.GetPrimaryKeyColumnName(val)
-	if pName == nil {
-		panic("primary key is required.")
-	}
-	sqlstring := sqlutils.BuildSelectClause(val) + fmt.Sprintf(" WHERE %s = $1 LIMIT 1", *pName)
-	rows, err := db.Query(sqlstring, pkId)
+func queryAndFill(db *sql.DB, val interface{}, sqlstring string, args ...interface{}) *Result {
+	rows, err := db.Query(sqlstring, args...)
 	if err != nil {
 		return NewErrorResult(err, sqlstring)
 	}
-
 	defer rows.Close()
 
 	if rows.Next() {
@@ -35,32 +28,19 @@ func Load(db *sql.DB, val interface{}, pkId int64) *Result {
 	return res
 }
 
+// Load record by primary key value.
+func Load(db *sql.DB, val interface{}, pkId int64) *Result {
+	pName := sqlutils.GetPrimaryKeyColumnName(val)
+	if pName == nil {
+		panic("primary key is required.")
+	}
+	sqlstring := sqlutils.BuildSelectClause(val) + fmt.Sprintf(" WHERE %s = $1 LIMIT 1", *pName)
+	return queryAndFill(db, val, sqlstring, pkId)
+}
+
 func LoadByCols(db *sql.DB, val interface{}, cols map[string]interface{}) *Result {
 	sqlstring := sqlutils.BuildSelectClause(val)
 	whereSql, args := sqlutils.BuildWhereClauseWithAndOp(cols)
-
 	sqlstring += whereSql
-
-	rows, err := db.Query(sqlstring, args...)
-	if err != nil {
-		return NewErrorResult(err, sqlstring)
-	}
-
-	defer rows.Close()
-
-	if rows.Next() {
-		err = sqlutils.FillFromRow(val, rows)
-		if err != nil {
-			return NewErrorResult(err, sqlstring)
-		}
-		return NewResult(sqlstring)
-	}
-	err = rows.Err()
-	if err != nil {
-		return NewErrorResult(err, sqlstring)
-	}
-
-	res := NewResult(sqlstring)
-	res.IsEmpty = true
-	return res
+	return queryAndFill(db, val, sqlstring, args...)
 }
