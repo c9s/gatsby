0429a2e271139414acc17484a293e80b93c9cfac
c9s <cornelius.howl@gmail.com>
2013-09-20 13:22:15 +0800

    improve checker loop time by -58% delta

--- sqlutils/checker.go
+++ sqlutils/checker.go
@@ -1,7 +1,11 @@
 package sqlutils
 
-import "errors"
-import "reflect"
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"strings"
+)
 
 // struct pointer
 func CheckRequired(val interface{}) error {
@@ -10,16 +14,26 @@ func CheckRequired(val interface{}) error {
 
 	for i := 0; i < t.NumField(); i++ {
 		var tag reflect.StructTag = typeOfT.Field(i).Tag
-		var attributes map[string]bool = GetColumnAttributesFromTag(&tag)
-		if _, ok := attributes["required"]; ok {
+		var tagStr = tag.Get("field")
+
+		// var attributes map[string]bool = GetColumnAttributesFromTag(&tag)
+		// if _, ok := attributes["required"]; ok {
+		if p := strings.Index(tagStr, "required"); p != -1 {
 			var fieldValue reflect.Value = t.Field(i)
 			var fieldType = typeOfT.Field(i)
+			var val = fieldValue.Interface()
 
-			// check the column value
-			if fieldValue.Type().Name() == "string" && fieldValue.Interface().(string) == "" {
-				return errors.New(fieldType.Name + " field is required.")
-			} else if fieldValue.Type().Name() == "int" && fieldValue.Interface().(int) == 0 {
-				return errors.New(fieldType.Name + " field is required.")
+			switch t := val.(type) {
+			default:
+				fmt.Printf("unexpected type %T", t) // %T prints whatever type t has
+			case string:
+				if t == "" {
+					return errors.New(fieldType.Name + " field is required.")
+				}
+			case int:
+				if t == 0 {
+					return errors.New(fieldType.Name + " field is required.")
+				}
 			}
 		}
 	}
