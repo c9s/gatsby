1adf6b74b0f5909b1e6575e4ca1e9ccc28b72c5f
c9s <cornelius.howl@gmail.com>
2013-09-21 14:36:52 +0800

    Improve CheckRequired performance

--- sqlutils/checker.go
+++ sqlutils/checker.go
@@ -7,7 +7,6 @@ import (
 	"strings"
 )
 
-// struct pointer
 func CheckRequired(structVal interface{}) error {
 	t := reflect.ValueOf(structVal).Elem()
 	typeOfT := t.Type()
@@ -16,29 +15,30 @@ func CheckRequired(structVal interface{}) error {
 	var tagStr string
 	var p int
 
-	var fieldValue reflect.Value
 	var fieldType reflect.StructField
 	var val interface{}
 	for i := 0; i < t.NumField(); i++ {
 		tag = typeOfT.Field(i).Tag
-		tagStr = tag.Get("field")
+		if tagStr = tag.Get("field"); len(tagStr) == 0 {
+			continue
+		}
 
 		// var attributes map[string]bool = GetColumnAttributesFromTag(&tag)
 		// if _, ok := attributes["required"]; ok {
-		if p = strings.Index(tagStr, "required"); p != -1 {
-			fieldValue = t.Field(i)
-			fieldType = typeOfT.Field(i)
-			val = fieldValue.Interface()
+		if p = strings.Index(tagStr, "required"); p != -1 && p > 0 {
+			val = t.Field(i).Interface()
 
 			switch t := val.(type) {
 			default:
 				fmt.Printf("unexpected type %T", t) // %T prints whatever type t has
 			case string:
 				if t == "" {
+					fieldType = typeOfT.Field(i)
 					return errors.New(fieldType.Name + " field is required.")
 				}
 			case int, int16, int32, int64:
 				if t == 0 {
+					fieldType = typeOfT.Field(i)
 					return errors.New(fieldType.Name + " field is required.")
 				}
 			}
